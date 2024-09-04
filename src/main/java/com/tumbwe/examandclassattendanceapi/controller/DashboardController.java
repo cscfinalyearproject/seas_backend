097d7b654f651b16201e8b212f495f9abfcf22b4
ro35ert <trebrorobertsbob@gmail.com>
2024-09-04 11:52:17 +0300

    cors

--- src/main/java/com/tumbwe/examandclassattendanceapi/controller/DashboardController.java
+++ src/main/java/com/tumbwe/examandclassattendanceapi/controller/DashboardController.java
@@ -4,14 +4,12 @@ package com.tumbwe.examandclassattendanceapi.controller;
 import com.tumbwe.examandclassattendanceapi.service.DashboardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/dashboard")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class DashboardController {
 
     private final DashboardService dashboardService;
